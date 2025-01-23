8a917f3ac02209035c673abc4469911c71c24a09
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-01-23 11:47:01 +0100

    exo trie a bulle fonctionne

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] tableau = {6, 5, 3, 8, 1, 4, 9, 7, 11 , 2, 10};
+
+        triABulle(tableau);
+
+        System.out.println("Tableau trié : ");
+        for (int i : tableau) {
+            if (i == tableau.length) {
+                System.out.print(i);
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    public static void triABulle(int[] tableau) {
+        for (int i = tableau.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (tableau[j + 1] < tableau[j]) {
+                    int temp = tableau[j + 1];
+                    tableau[j + 1] = tableau[j];
+                    tableau[j] = temp;
+                }
+            }
+        }
     }
 }
