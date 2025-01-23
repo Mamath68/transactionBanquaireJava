dc893d63f8e5312777c0bb9c31f7bd7f03527e2c
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-01-23 11:47:37 +0100

    correction

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int[] tableau = {6, 5, 3, 8, 1, 4, 9, 7, 11 , 2, 10};
 
-        triABulle(tableau);
+        trieABulle(tableau);
 
         System.out.println("Tableau trié : ");
         for (int i : tableau) {
@@ -14,7 +14,7 @@ public class Main {
         }
     }
 
-    public static void triABulle(int[] tableau) {
+    public static void trieABulle(int[] tableau) {
         for (int i = tableau.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (tableau[j + 1] < tableau[j]) {
